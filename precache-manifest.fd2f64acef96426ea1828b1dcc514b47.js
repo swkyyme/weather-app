a40e8b465b5322b39a5ae8c1131f6a7dbd36b881
Skye <wym0529@hotmail.com>
2020-01-21 10:08:38 +1000

    Updates

---
+++ precache-manifest.fd2f64acef96426ea1828b1dcc514b47.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "468f348b8079bb03824543ff8d7671b7",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "5d732bfaf7c8e8d6f2f6",
+    "url": "/weather-app/static/css/2.d62e3072.chunk.css"
+  },
+  {
+    "revision": "88c0660073900a327c2c",
+    "url": "/weather-app/static/css/main.d6edae43.chunk.css"
+  },
+  {
+    "revision": "5d732bfaf7c8e8d6f2f6",
+    "url": "/weather-app/static/js/2.0c4fdaef.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/weather-app/static/js/2.0c4fdaef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "88c0660073900a327c2c",
+    "url": "/weather-app/static/js/main.835b4757.chunk.js"
+  },
+  {
+    "revision": "f6e79f44ce08fb03d80e",
+    "url": "/weather-app/static/js/runtime-main.bf761b10.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/weather-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/weather-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/weather-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/weather-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/weather-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
